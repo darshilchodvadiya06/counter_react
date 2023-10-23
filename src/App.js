5337c354da9775ce1086898b8f3be6780fc375a3
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-10-23 14:33:24 +0530

    counter react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(0);
+
+  const increment = () => {
+    setData(data + 1);
+  };
+  const decrement = () => {
+    setData(data - 1);
+  };
+  useEffect(() => {
+    alert("show my counter...") 
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>✨ System ✨</h1>
+          <button onClick={increment}>Increment</button>
+          <p>Counter:{data}</p>
+          <button onClick={decrement}>Decrement</button>
+
+        </div>
       </header>
     </div>
   );
